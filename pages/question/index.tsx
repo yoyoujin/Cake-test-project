9f30479df0f487fd61f2d6bbb0a5933397520e9e
youjin <yjinhann@gmail.com>
2023-05-08 15:04:27 +0900

    feat: 더미데이터 생성

--- pages/question/index.tsx
+++ pages/question/index.tsx
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const QuestionPage = () => {
-  return <div>QuestionPage</div>;
+interface Question {
+  id: number;
+  question: string;
+  options: string[];
+}
+
+const QuestionPage: React.FC = () => {
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchQuestion = async () => {
+      try {
+        const response = await fetch('/data/quiz.json');
+        const data = await response.json();
+        setQuestions(data.questions);
+        setLoading(false);
+      } catch (error) {
+        console.log('Error fetching question data:', error);
+      }
+    };
+
+    fetchQuestion();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      {questions.map((question) => (
+        <div key={question.id}>
+          <p>{question.question}</p>
+          <ul>
+            {question.options.map((option) => (
+              <li key={option}>{option}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default QuestionPage;
