442bfe3c8a1a7140fb2d6e7509f29951d8e2d3a9
youjin <yjinhann@gmail.com>
2023-05-09 12:21:39 +0900

    feat: 홈에서 문제 페이지로 이동하는  핸들링 함수 연결

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
+import Router, { useRouter } from 'next/router';
+
 export default function Home() {
+  const router = useRouter();
+  const handleQuestionRedirect = () => {
+    router.push('/question');
+  };
   return (
     <>
       <h1>Start!</h1>
+      <button onClick={handleQuestionRedirect}>내 케이크 픽업하러가기 🍰</button>
     </>
   );
 }
