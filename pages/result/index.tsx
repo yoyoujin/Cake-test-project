50b54bb671c947e3244dfcff82d3a9bbd4fd3567
youjin <yjinhann@gmail.com>
2023-05-10 19:35:36 +0900

    feat: usernameAtom ResultPage로 값 가져오기

--- pages/result/index.tsx
+++ pages/result/index.tsx
@@ -1,7 +1,18 @@
+import { usernameAtom } from '@/recoil/atoms';
 import React from 'react';
+import { useRecoilValue } from 'recoil';
 
 const ResultPage = () => {
-  return <h1>ResultPage</h1>;
+  const username = useRecoilValue(usernameAtom);
+
+  console.log(username[0]);
+  console.log(username[1]);
+  return (
+    <>
+      <h1>ResultPage</h1>
+      <p>{username}</p>
+    </>
+  );
 };
 
 export default ResultPage;
